34346b9eadb0bb6c0067f38423846482e9d28665
David Urting <david.urting@live.com>
2021-01-19 18:33:31 +0100

    Add project files.

---
+++ ToDo.Web/Controllers/ToDoController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using ToDo.Web.Models;
+
+namespace ToDo.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+        // Beter (ipv static variabelen) is natuurlijk een database gebruiken (via Entity Framework).
+        // Hieronder wordt een 'Dicationary' gebruikt: dat is een structuur waarin je met een sleutel
+        // op zoek kan gaan naar een overeenkomstige waarde. Je kan het zien als een woordenboek
+        // waarbij de 'key' het woord is, en de 'value' is het de verklaring van dat woord.
+        private static Dictionary<int, ToDoItem> _toDoItems = new Dictionary<int, ToDoItem>();
+
+        /// <summary>
+        /// Ophalen van alle ToDo items via een GET naar /url/todo
+        /// </summary>
+        /// <returns>JSON met array van ToDoItem objecten.</returns>
+        [HttpGet]
+        public IEnumerable<ToDoItem> Get()
+        {
+            return _toDoItems.Values;
+        }
+
+        /// <summary>
+        /// Ophalen van één specifieke ToDo item via een GET naar /url/todo/{id}.
+        /// </summary>
+        /// <param name="id">Een getal vanaf 0.</param>
+        /// <returns>JSON met een ToDoItem object</returns>
+        [HttpGet("{id}")]
+        public ToDoItem Get(int id)
+        {
+            if (_toDoItems.ContainsKey(id))
+            {
+                return _toDoItems[id];
+            }
+            else return null;
+        }
+
+        /// <summary>
+        /// Aanmaken (onthouden) van een nieuwe ToDo item komende van de front end.
+        /// </summary>
+        /// <param name="toDoItem">JSON met ToDoItem object. De Id van het object stel je best in op -1, maar eigenlijk doet het er niet echt toe.</param>
+        /// <returns>JSON met daarin een number (het id).</returns>
+        [HttpPost]
+        public int Post([FromBody] ToDoItem toDoItem)
+        {
+            // Zoals je ziet is de backend verantwoordelijk voor het genereren van een ID.
+            toDoItem.Id = ToDoItem.GetNextId(); // Unieke sleutel toekennen (normaal doet de database dat, of gebruik je een GUID).
+            _toDoItems.Add(toDoItem.Id, toDoItem);
+            return toDoItem.Id; // id terugsturen zodat de browser te weten kan komen welke ID werd toegekend.
+        }
+
+        /// <summary>
+        /// Bijwerken van een reeds bestaande ToDoItem object.
+        /// De backend zal op basis van het 'Id' op zoek gaan naar het overeenkomstige object in de dictionary.
+        /// </summary>
+        /// <param name="toDoItem">ToDoItem dat moet bijgewerkt worden. Indien het Id niet gevonden wordt gebeurt er niets.</param>
+        [HttpPut()]
+        public void Put([FromBody] ToDoItem toDoItem)
+        {
+            // PUT: bijwerken
+            if (_toDoItems.ContainsKey(toDoItem.Id))
+            {
+                _toDoItems[toDoItem.Id].Titel = toDoItem.Titel;
+                _toDoItems[toDoItem.Id].Omschrijving = toDoItem.Omschrijving;
+                _toDoItems[toDoItem.Id].ToegewezenAan = toDoItem.ToegewezenAan;
+                _toDoItems[toDoItem.Id].Deadline = toDoItem.Deadline;
+            }            
+        }
+
+        /// <summary>
+        /// Verwijderen van een ToDoItem met de specifieke Id.
+        /// Indien er geen ToDoItem werd gevonden gebeurt er niets.
+        /// </summary>
+        /// <param name="id">JSON met daarin een number (het id).</param>
+        [HttpDelete()]
+        public void Delete([FromBody] int id)
+        {
+            if (_toDoItems.ContainsKey(id))
+            {
+                _toDoItems.Remove(id);
+            }
+        }
+    }
+}
---
+++ ToDo.Web/Models/ToDoItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ToDo.Web.Models
+{
+    public class ToDoItem
+    {
+        private static int _nexId = 0;
+
+        /// <summary>
+        /// Met deze 'helper' methode kan de controller unieke ID's genereren voor ToDoItem's
+        /// die via de POST binnenkomen.
+        /// </summary>
+        /// <returns>Unieke ID. </returns>
+        public static int GetNextId()
+        {
+            return _nexId++;
+        }
+
+        public int Id { get; set; }
+
+        public string Titel { get; set; }
+        public string Omschrijving { get; set; }
+        public string ToegewezenAan { get; set; }
+        public DateTime? Deadline { get; set; }
+    }
+}
